c15eb3a66d31d8dbe3578e54bc9d33f39c726cae
phuhuutin <39242538+phuhuutin@users.noreply.github.com>
2024-01-11 15:37:11 -0800

    Added enviromentEntity

---
+++ enviromentEntity/farmLandNormalGrass.js
@@ -0,0 +1,23 @@
+class FarmLandNormalGrass{
+    constructor(game, x, y) {
+        Object.assign(this, { game, x, y });
+
+        this.x = 0;
+        this.y = 0;
+        // spritesheet
+       // this.spritesheet = ASSET_MANAGER.getAsset("./sprites/grassbase.png");
+    }; 
+
+    update(){
+
+    };
+    draw(ctx) {
+   
+     
+        ctx.fillStyle = "#367f63";
+        ctx.fillRect(0- this.game.camera.x, - this.game.camera.y, 1500, 2000);
+     
+    }
+
+}
+ 
