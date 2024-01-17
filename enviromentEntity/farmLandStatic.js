90110264e529eb50bebdfff1b9e20ab5c3abd0d6
NunuSnowman <lixin.wang.cn@gmail.com>
2024-01-17 00:47:12 -0800

    add more trees and animals

--- enviromentEntity/farmLandStatic.js
+++ enviromentEntity/farmLandStatic.js
@@ -1,8 +1,9 @@
 const StaticType = {
-        ANIMAL_WATER: 'animalWater',
-        ANIMAL_Food: 'animalFood',
-    };
-    
+        ANIMAL_WATER: 'ANIMAL_WATER',
+        ANIMAL_Food: 'ANIMAL_Food',
+
+};
+
 class FarmLandStatic {
         constructor(game, x, y, type) {
                 Object.assign(this, { game, x, y });
@@ -10,22 +11,24 @@ class FarmLandStatic {
                 this.spritesheet = ASSET_MANAGER.getAsset("./sprites/farmland.png");
         }
         update() {
-            // Add your update logic here if needed
+                // Add your update logic here if needed
         }
         draw(ctx) {
                 // Draw the static image
-                if(this.type == StaticType.ANIMAL_Food){
-                        ctx.drawImage(this.spritesheet, 707, 320, 59, 31,
-                        this.x - this.game.camera.x,
-                        this.y - this.game.camera.y,
-                        70, 40);
-                }else if(this.type == StaticType.ANIMAL_WATER){
+                switch (this.type) {
+                        case StaticType.ANIMAL_Food:
+                                ctx.drawImage(this.spritesheet, 707, 320, 59, 31,
+                                        this.x - this.game.camera.x,
+                                        this.y - this.game.camera.y,
+                                        70, 40);
+                                break;
+                        case StaticType.ANIMAL_WATER:
+                                ctx.drawImage(this.spritesheet, 545, 226, 30, 57,
+                                        this.x - this.game.camera.x,
+                                        this.y - this.game.camera.y,
+                                        30, 50);
+                                break;
 
-                        ctx.drawImage(this.spritesheet, 545, 226, 30, 57,
-                                this.x - this.game.camera.x ,
-                                this.y - this.game.camera.y ,
-                                30, 50);
                 }
         }
 }
-    
