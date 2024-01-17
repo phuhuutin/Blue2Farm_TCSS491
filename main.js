d9ac78b64858e57a75b149996a916f43f9704698
phuhuutin <phuhuutin@gmail.com>
2024-01-16 18:07:48 -0800

    load Boar Idle sprite

--- main.js
+++ main.js
@@ -17,12 +17,14 @@ ASSET_MANAGER.queueDownload("./sprites/enemy/wizardRun.png");
 ASSET_MANAGER.queueDownload("./sprites/enemy/wizardAttack1.png");
 ASSET_MANAGER.queueDownload("./sprites/enemy/boarWalk.png");
 ASSET_MANAGER.queueDownload("./sprites/enemy/boarThrust.png");
+ASSET_MANAGER.queueDownload("./sprites/enemy/boarIdle.png");
 ASSET_MANAGER.queueDownload("./sprites/enemy/goblinattack.png");
 ASSET_MANAGER.queueDownload("./sprites/enemy/goblinrun.png");
 ASSET_MANAGER.queueDownload("./sprites/enemy/greengoblin.png");
 ASSET_MANAGER.queueDownload("./sprites/enemy/greengoblin_attack.png");
 ASSET_MANAGER.queueDownload("./sprites/enemy/wormattack.png");
 ASSET_MANAGER.queueDownload("./sprites/enemy/wormwalk.png");
+
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
