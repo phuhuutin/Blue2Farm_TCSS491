88f8ed4425b0a204b549632afd58a42dc6155f66
phuhuutin <phuhuutin@gmail.com>
2024-01-26 15:54:13 -0800

    Adjusted for Prototype Showcase.

--- dayNightCycle.js
+++ dayNightCycle.js
@@ -2,9 +2,9 @@ class DayNightCycle {
     constructor(game, time) {
         Object.assign(this, { game, time });
         this.game = game;
-        this.time = time; // Time of day (0 to 24, where 0 is midnight and 12 is noon)
-        this.brightRadius = 100; // Radius of the bright circle
-        this.glowRadius = 50; // Radius of the glow effect
+        this.time = time;
+        this.brightRadius = 100; 
+        this.glowRadius = 50;
     }
 
     update() {
@@ -17,15 +17,12 @@ class DayNightCycle {
     }
 
     draw(ctx) {
-         // Adjust visuals based on time of day
+        
          let nightAlpha = 0;
-
-         // Make it darker at night
          if (this.time >= 18 || this.time < 6) {
-             nightAlpha = Math.abs(this.time - 12) / 6; // Fade in/out based on time
-               // Light-up effect around the main character
+             nightAlpha = Math.abs(this.time - 12) / 6; 
+              
          }
-
          if(this.time >= 20 || this.time < 4){
             const characterX = this.game.character.x - this.game.camera.x;
             const characterY = this.game.character.y - this.game.camera.y;
@@ -37,8 +34,6 @@ class DayNightCycle {
                 characterY,
                 200 // Increased radius for a stronger glow
             );
-
-
 
             gradient.addColorStop(0, 'rgba(255, 255, 255, 0.1)'); // Adjust color and opacity
             gradient.addColorStop(0.5, 'rgba(255, 255, 255, 0.4)'); // Adjust color and opacity
@@ -54,18 +49,9 @@ class DayNightCycle {
 
         // Draw dark overlay during the night
         if (nightAlpha > 0.5) {
-          
-             // Draw the overlay as a single large circle
-             ctx.globalAlpha = nightAlpha - 0.5; // Adjust the starting alpha for a smoother transition
-             ctx.fillStyle = 'rgba(0, 0, 0, 0.9)'; // Adjust the color and opacity of the overlay
- 
- 
-    
+             ctx.globalAlpha = nightAlpha - 0.5; 
+             ctx.fillStyle = 'rgba(0, 0, 0, 0.9)'; 
              ctx.fillRect(0, 0, PARAMS.CANVAS_WIDTH, PARAMS.CANVAS_HEIGHT + this.game.character.y + this.game.camera.y);
-        
-                            
-           
-
             }
  
         ctx.globalAlpha = 1; // Reset global alpha
